695efc9275a803bfe7b88e9231722036e111f556
Konstantin <Kpstya@mail.ru>
2022-10-26 23:05:47 +0300

    fix sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="border-end bg-white" id="sidebar-wrapper">
-    <div class="sidebar-heading border-bottom bg-light">Тестовое задание</div>
+    <div class="sidebar-heading border-bottom bg-light">Пульт админа</div>
     <div class="list-group list-group-flush">
         <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ route('index', ['filter' => '0']) }}">Все задачи</a>
         <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ route('myTasks', ['userId' => Auth::user()->id]) }}">Мои задачи</a>
